dd5f051600e621a56a8d3101a5170f7ff4ff1ca9
Muhammad Umer Siddiqui <134856263+Fateelixir@users.noreply.github.com>
2026-09-02 23:50:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector(".main-header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    }
+
+    else{
+        header.classList.remove("scrolled");
+    }
+
+});
